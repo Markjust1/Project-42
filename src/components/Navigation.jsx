8d71354e07b1b2eb88ef545f8fe616d9644094be
Markjust1 <novikov711@gmail.com>
2022-10-31 13:03:20 -0600

    add background

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,13 +1,25 @@
 import "./Navigation.css";
+import logo from "../assets/game-icon.png";
+import search from "../assets/search.png";
+import profile from "../assets/profile.png";
+import cart from "../assets/cart.png";
 
 const Navigation = () => {
   return (
     <nav className="navbar-container">
-      <div className="navbar-title">Project-42</div>
+      <div className="navbar-title">
+        <img src={logo} alt="Site logo"></img>
+      </div>
       <div className="navbar-elements">
-        <div className="navbar-search">Search</div>
-        <div className="navbar-profile">My Profile</div>
-        <div className="navbar-sell">Sell +</div>
+        <div className="navbar-search">
+          <img src={search} alt="Search"></img>
+        </div>
+        <div className="navbar-profile">
+          <img src={profile} alt="Profile"></img>
+        </div>
+        <div className="navbar-cart">
+          <img src={cart} alt="Cart"></img>
+        </div>
       </div>
     </nav>
   );
