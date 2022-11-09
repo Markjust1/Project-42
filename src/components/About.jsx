4b5ecacb8f60b21a91c69ce232d6b810c74ca88d
Markjust1 <novikov711@gmail.com>
2022-11-08 21:05:57 -0700

    add about component

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import "./styles/About.css";
+
+const About = () => {
+
+  return (
+    <div className="about-container">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa facere placeat dignissimos rem odit. Atque quasi dignissimos aliquid corrupti nulla dicta architecto vel beatae, iusto harum quidem maxime illum libero?</p>
+    </div>
+  );
+};
+
+export default About;
