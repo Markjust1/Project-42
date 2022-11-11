3198bee95cecec6951ff7235a6104751b4439e5a
Markjust1 <novikov711@gmail.com>
2022-11-10 19:12:50 -0700

    add terms and cond component

---
+++ src/components/Terms.jsx
@@ -0,0 +1,12 @@
+import "./styles/Terms.css";
+
+const Terms = () => {
+
+  return (
+    <div className="terms-container">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa facere placeat dignissimos rem odit. Atque quasi dignissimos aliquid corrupti nulla dicta architecto vel beatae, iusto harum quidem maxime illum libero?Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa facere placeat dignissimos rem odit. Atque quasi dignissimos aliquid corrupti nulla dicta architecto vel beatae, iusto harum quidem maxime illum libero?Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa facere placeat dignissimos rem odit. Atque quasi dignissimos aliquid corrupti nulla dicta architecto vel beatae, iusto harum quidem maxime illum libero?</p>
+    </div>
+  );
+};
+
+export default Terms;
