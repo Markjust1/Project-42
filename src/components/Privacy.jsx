1b6da9b9c0cb8a8c30658908f4656d77aa69e41e
Markjust1 <novikov711@gmail.com>
2022-11-14 10:38:57 -0700

    add privacy comp

---
+++ src/components/Privacy.jsx
@@ -0,0 +1,12 @@
+import "./styles/Privacy.css";
+
+const Privacy = () => {
+
+  return (
+    <div className="privacy-container">
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo odit, sequi error temporibus ipsum enim velit magni voluptatum fugiat facere numquam expedita autem iure dicta consequuntur, veritatis placeat iusto in deserunt quia nam. Cupiditate maxime quibusdam fugit atque distinctio soluta.</p>
+    </div>
+  );
+};
+
+export default Privacy;
