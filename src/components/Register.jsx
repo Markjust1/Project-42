71134679cb8473b15b317f56c0f0352099b0a178
Markjust1 <novikov711@gmail.com>
2022-12-13 11:53:11 -0700

    adding logic to username and password validation

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,14 +1,59 @@
 import { useRef, useState, useEffect } from "react";
-import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faCheck,
+  faTimes,
+  faInfoCircle,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./styles/Register.css";
 
+const user_regex = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
+const password_regex =
+  /^(?=.*[a-z])(?=.*[A-Z](?=.*[0-9])(?=.*[!@#$%])).{8,24}$/;
+
 const Register = () => {
-  return (
-    <div className="register-container">
-      
-    </div>
-  );
+  const userRef = useRef();
+  const errRef = useRef();
+  // State for username
+  const [user, setUser] = useState("");
+  const [validName, setValidName] = useState(false);
+  const [userFocus, setUserFocus] = useState(false);
+  // State for password
+  const [pwd, setpwd] = useState("");
+  const [validPwd, setValidPwd] = useState(false);
+  const [pwdFocus, setPwdFocus] = useState(false);
+  // State for matching password
+  const [matchPwd, setMatchPwd] = useState("");
+  const [validMatch, setValidMatch] = useState(false);
+  const [matchFocus, setMatchFocus] = useState(false);
+  // State for error message
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  // For setting the focus when the component loads
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  // UseEffect for username
+  useEffect(() => {
+    const result = user_regex.test(user);
+    console.log(result);
+    console.log(user);
+    setValidName(result);
+  }, [user]);
+
+  // UseEffect for the password
+  useEffect(() => {
+    const result = pwd_regex.test(pwd);
+    console.log(result);
+    console.log(pwd);
+    setValidPwd(result);
+    const match = pwd === matchPwd;
+    setValidMatch(match);
+  }, [pwd, matchPwd]);
+
+  return <div className="register-container"></div>;
 };
 
 export default Register;
