61620819f33ff85a071730550d9ed2737dc17518
Markjust1 <novikov711@gmail.com>
2022-12-23 16:25:09 -0700

    setting up mongodb database connections

---
+++ express-backend/server.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+const express = require("express");
+const app = express();
+const PORT = 3000;
+const mongoose = require("mongoose");
+
+// Connecting mongoose db
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Database connected!"));
+
+app.listen(PORT, () => {
+  console.log(`Server has started on port: ${PORT}`);
+});
