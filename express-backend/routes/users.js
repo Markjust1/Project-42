672eec018f61dbfe745ef8369895aa5276ac8a66
Markjust1 <novikov711@gmail.com>
2022-12-23 18:41:13 -0700

    add delete user route

--- express-backend/routes/users.js
+++ express-backend/routes/users.js
@@ -11,26 +11,52 @@ router.get("/", async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 });
+
 // Get one user
-router.get("/:id", (req, res) => {
-  res.send(req.params.id);
+router.get("/:id", getUser, (req, res) => {
+  res.json(res.user);
 });
+
 // Create user
 router.post("/", async (req, res) => {
   const user = new User({
     name: req.body.name,
     email: req.body.email,
-  })
+  });
 
   try {
     const newUser = await user.save();
     res.status(201).json(newUser);
   } catch (err) {
-    res.status(400).json({message: err.message})
+    res.status(400).json({ message: err.message });
   }
 });
+
 // Update/modify user
-router.patch("/", (req, res) => {});
+router.patch("/", getUser, (req, res) => {});
+
 // Delete user
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", getUser, async (req, res) => {
+  try {
+    await res.user.remove();
+    res.json({ message: "Deleted user" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+async function getUser(req, res, next) {
+  let user;
+  try {
+    user = await User.findById(req.params.id);
+    if (user == null) {
+      return res.status(404).json({ message: "Cannot find user" });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+  res.user = user;
+  next();
+}
+
 module.exports = router;
