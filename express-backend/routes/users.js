d2a3c4bb8dbbced54149aa3dbca624f2da50959f
Markjust1 <novikov711@gmail.com>
2022-12-23 17:28:44 -0700

    add rest route templates

--- express-backend/routes/users.js
+++ express-backend/routes/users.js
@@ -1,4 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
+// Get all users
+router.get('/', (req, res) => {
+  res.send('all users')
+}) 
+// Get one user
+router.get('/:id', (req, res) => {
+  res.send(req.params.id)
+})
+// Create user
+router.post('/', (req, res) => {
+
+})
+// Update/modify user
+router.patch('/', (req, res) => {
+
+})
+// Delete user
+router.delete('/:id', (req, res) => {
+
+}) 
 module.exports = router;
