92c586b4b93f897103e852373852ef535249b848
Markjust1 <novikov711@gmail.com>
2022-12-23 17:40:14 -0700

    logic for get all users route

--- express-backend/routes/users.js
+++ express-backend/routes/users.js
@@ -1,24 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const User = require("../models/user");
 
 // Get all users
-router.get('/', (req, res) => {
-  res.send('all users')
-}) 
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 // Get one user
-router.get('/:id', (req, res) => {
-  res.send(req.params.id)
-})
+router.get("/:id", (req, res) => {
+  res.send(req.params.id);
+});
 // Create user
-router.post('/', (req, res) => {
-
-})
+router.post("/", (req, res) => {});
 // Update/modify user
-router.patch('/', (req, res) => {
-
-})
+router.patch("/", (req, res) => {});
 // Delete user
-router.delete('/:id', (req, res) => {
-
-}) 
+router.delete("/:id", (req, res) => {});
 module.exports = router;
