b62aca1c5fa20544ad7d0426a21bdd8d85dac06e
Markjust1 <novikov711@gmail.com>
2022-12-23 17:55:52 -0700

    add create user route

--- express-backend/routes/users.js
+++ express-backend/routes/users.js
@@ -16,7 +16,19 @@ router.get("/:id", (req, res) => {
   res.send(req.params.id);
 });
 // Create user
-router.post("/", (req, res) => {});
+router.post("/", async (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+  })
+
+  try {
+    const newUser = await user.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(400).json({message: err.message})
+  }
+});
 // Update/modify user
 router.patch("/", (req, res) => {});
 // Delete user
