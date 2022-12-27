e36b20d91da219b7194bc3e7860a116ec11feadb
Markjust1 <novikov711@gmail.com>
2022-12-27 11:16:23 -0700

    add get all and one item

--- express-backend/routes/items.js
+++ express-backend/routes/items.js
@@ -6,13 +6,35 @@ const Item = require('../models/item');
 
 router.get('/', async (req,res) => {
   try {
-
-  } catch {
-    
+    const items = await Item.find();
+    res.json(items)
+  } catch (err){
+    res.status(404).json({message: err.message})
   }
 });
 
 // Get one item
+
+router.get('/:id', getItem, (req, res) => {
+  res.json(res.item)
+})
+
 // Create one
 // Update one
 // Delete one
+
+// Middleware function
+
+async function getItem(req, res, next) {
+  let item;
+  try {
+    item = await Item.findById(req.params.id);
+    if (item == null) {
+      return res.status(404).json({message: "Cannot find item"})
+    }
+  } catch (err){
+    return res.status(500).json({message: err.message})
+  }
+  res.item = item;
+  next();
+}
