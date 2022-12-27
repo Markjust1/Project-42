7e12ebb2bd8ddc385aac4c9f5c5e8a211a9de10f
Markjust1 <novikov711@gmail.com>
2022-12-27 10:47:43 -0700

    add db schema for items

---
+++ express-backend/routes/items.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const Item = require('../models/item');
+
+// Get all items
+
+router.get('/', async (req,res) => {
+  try {
+
+  } catch {
+    
+  }
+});
+
+// Get one item
+// Create one
+// Update one
+// Delete one
--- express-backend/server.js
+++ express-backend/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 2500;
 const mongoose = require("mongoose");
 
 // Connecting mongoose db
