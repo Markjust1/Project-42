4d6d9945c19d61ccfe079bf87241d2d7f33c1472
Markjust1 <novikov711@gmail.com>
2023-01-19 11:38:23 -0700

    complete styling card

--- react-frontend/src/components/Premium_item.jsx
+++ react-frontend/src/components/Premium_item.jsx
@@ -4,14 +4,14 @@ const Premium_Item = (props) => {
   console.log("received props", props);
   return (
     <>
-    <div className="premium-container">
-      <div className="premium-title">{props.title}</div>
-      <img className="premium-image" src={props.image} alt="game image" />
-      <div className="premium-description">{props.description}</div>
-    </div>
-    <div className="price-circle">
-      <div className="premium-price">${props.price}</div>
-    </div>
+      <div className="premium-container">
+        <div className="premium-title">{props.title}</div>
+        <img className="premium-image" src={props.image} alt="game image" />
+        <div className="premium-description">{props.description}</div>
+        <div className="price-circle">
+          <div className="premium-price">${props.price}</div>
+        </div>
+      </div>
     </>
   );
 };
