6682e96c41918ace87e8b5b3be25ed5d1cc52818
Markjust1 <novikov711@gmail.com>
2023-01-19 22:19:06 -0700

    change navbar styling

--- react-frontend/src/components/Navigation.jsx
+++ react-frontend/src/components/Navigation.jsx
@@ -1,37 +1,33 @@
 import { Link } from "react-router-dom";
-import logo from "../assets/game-icon.png";
-import home from "../assets/home.png";
-import profile from "../assets/profile.png";
-import add from "../assets/add.png";
-import Title from "./Title";
+// import logo from "../assets/game-icon.png";
+// import home from "../assets/home.png";
+// import profile from "../assets/profile.png";
+// import add from "../assets/add.png";
+// import Title from "./Title";
 import "./styles/Navigation.css";
 
 const Navigation = () => {
   return (
     <nav className="navbar-container">
-      <Title />
+      <div className="navbar-title">PROJECT42</div>
       <div className="navbar-elements">
         <Link to="/" style={{ textDecoration: "none" }}>
           <div className="navbar-item">
-            <img src={home} alt="home"></img>
             <span>HOME</span>
           </div>
         </Link>
         <Link to="/login" style={{ textDecoration: "none" }}>
           <div className="navbar-item">
-            <img src={profile} alt="Login"></img>
             <span>LOGIN</span>
           </div>
         </Link>
         <Link to="/register" style={{ textDecoration: "none" }}>
           <div className="navbar-item">
-            <img src={profile} alt="Register"></img>
             <span>REGISTER</span>
           </div>
         </Link>
         <Link to="/add" style={{ textDecoration: "none" }}>
           <div className="navbar-item">
-            <img src={add} alt="Add Item"></img>
             <span>ADD ITEM</span>
           </div>
         </Link>
