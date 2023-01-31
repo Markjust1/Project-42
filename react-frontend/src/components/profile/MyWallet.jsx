5345789bd4793851350d360f7f0e28772324e141
Markjust1 <novikov711@gmail.com>
2023-01-31 10:59:26 -0700

    add form and content to mywallet comp

--- react-frontend/src/components/profile/MyWallet.jsx
+++ react-frontend/src/components/profile/MyWallet.jsx
@@ -1,9 +1,27 @@
 const MyWallet = () => {
   return (
     <div>
-      <div>Add new credit card</div>
-      <label></label>
-      <input></input>
+      <div>
+        <div>My Cards:</div>
+        <ul>
+          <li>Card 1</li>
+          <li>Card 2</li>
+          <li>Card 3</li>
+        </ul>
+      </div>
+
+      <div>Add new Card</div>
+      <form>
+        <label>Card number:</label>
+        <input></input>
+        <label>Cardholder name:</label>
+        <input></input>
+        <label>Expiry date:</label>
+        <input></input>
+        <label>CVS number:</label>
+        <input></input>
+      </form>
+
     </div>
   )
 };
