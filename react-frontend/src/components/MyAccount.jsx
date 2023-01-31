fc0c9f27b0681b66ea77ab5f9674a8390e612e1a
Markjust1 <novikov711@gmail.com>
2023-01-31 10:44:06 -0700

    ad conditional rendering on my account page

--- react-frontend/src/components/MyAccount.jsx
+++ react-frontend/src/components/MyAccount.jsx
@@ -1,23 +1,51 @@
 import "./styles/MyAccount.css";
-import { Link } from "react-router-dom";
 import EditProfile from "./profile/EditProfile";
 import MyWallet from "./profile/MyWallet";
 import MyItems from "./profile/MyItems";
 import MyOrders from "./profile/MyOrders";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const MyAccount = (props) => {
+  const [profile, setProfile] = useState(true);
+  const [wallet, setWallet] = useState(false);
+  const [myItems, setMyItems] = useState(false);
+  const [myOrders, setMyOrders] = useState(false);
+
+  const stateHandler = (exception) => {
+    console.log('exception state:',exception)
+    setProfile(false);
+    setWallet(false);
+    setMyItems(false);
+    setMyOrders(false);
+    const exemptionCall = new Function(eval(`set${exception}(true)`))
+    // console.log(typeof exemptionCall)
+    exemptionCall();
+  }
+
+  //   setProfile(false);
+  //   setWallet(false);
+  //   setMyItems(false);
+  //   setMyOrders(false);
+  // };
+
   return (
     <div className="cabinet-container">
       <div className="profile-separator">
         <div className="profile-picture"></div>
         <div className="options-list">
-          <EditProfile />
-          <MyWallet />
-          <MyItems />
-          <MyOrders />
+          <div onClick={() => stateHandler('Profile')}>Edit Profile</div>
+          <div onClick={() => stateHandler('Wallet')}>My Wallet</div>
+          <div onClick={() => stateHandler('MyItems')}>My Items</div>
+          <div onClick={() => stateHandler('MyOrders')}>My Orders</div>
         </div>
       </div>
-      <div className="my-items">My items</div>
+      <div className="side-panel">
+        {profile && <EditProfile />}
+        {wallet && <MyWallet />}
+        {myItems && <MyItems />}
+        {myOrders && <MyOrders />}
+      </div>
     </div>
   );
 };
