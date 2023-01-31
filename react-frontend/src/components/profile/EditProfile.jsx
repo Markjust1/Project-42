d1a4c35218324aec8ef29a9bf41b820630fbc3c7
Markjust1 <novikov711@gmail.com>
2023-01-31 11:34:24 -0700

    add basic structure to edit profile comp

--- react-frontend/src/components/profile/EditProfile.jsx
+++ react-frontend/src/components/profile/EditProfile.jsx
@@ -1,6 +1,40 @@
+import "../styles/profile-styles/EditProfile.css";
+
 const EditProfile = () => {
   return (
-    <div>Edit Profile</div>
+    <>
+    <div className='profile-title'>Edit My Profile</div>
+    <div className="profile-container">
+      {/* <div>
+        <div>My Cards:</div>
+        <ul>
+          <li>Card 1</li>
+          <button>remove</button>
+          <li>Card 2</li>
+          <button>remove</button>
+          <li>Card 3</li>
+          <button>remove</button>
+        </ul>
+      </div> */}
+      
+      <div>
+        <form>
+          <label>Full Name:</label>
+          <input></input>
+          <button>Edit</button>
+          <label>Email:</label>
+          <input></input>
+          <button>Edit</button>
+          <label>Username:</label>
+          <input></input>
+          <button>Edit</button>
+          <label>Avatar</label>
+          <input type='file'></input>
+          <button>Submit</button>
+        </form>
+      </div>
+    </div>
+    </>
   )
 };
 
