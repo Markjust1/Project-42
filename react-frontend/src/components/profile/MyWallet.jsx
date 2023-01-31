0f6982c4e5d1a8150647bb1be6a7ebd1e145fe09
Markjust1 <novikov711@gmail.com>
2023-01-31 11:24:50 -0700

    add basic elements to mywallet comp

--- react-frontend/src/components/profile/MyWallet.jsx
+++ react-frontend/src/components/profile/MyWallet.jsx
@@ -1,29 +1,38 @@
+import "../styles/profile-styles/MyWallet.css";
+
 const MyWallet = () => {
   return (
-    <div>
-      <div>
+    <>
+    <div className='wallet-title'>My Wallet</div>
+    <div className="wallet-container">
+      <div className="my-cards">
         <div>My Cards:</div>
         <ul>
           <li>Card 1</li>
+          <button>remove</button>
           <li>Card 2</li>
+          <button>remove</button>
           <li>Card 3</li>
+          <button>remove</button>
         </ul>
       </div>
-
-      <div>Add new Card</div>
-      <form>
-        <label>Card number:</label>
-        <input></input>
-        <label>Cardholder name:</label>
-        <input></input>
-        <label>Expiry date:</label>
-        <input></input>
-        <label>CVS number:</label>
-        <input></input>
-      </form>
-
+      
+      <div>
+        <div>Add new Card</div>
+        <form>
+          <label>Card number:</label>
+          <input></input>
+          <label>Cardholder name:</label>
+          <input></input>
+          <label>Expiry date:</label>
+          <input></input>
+          <label>CVS number:</label>
+          <input></input>
+        </form>
+      </div>
     </div>
-  )
+    </>
+  );
 };
 
 export default MyWallet;
