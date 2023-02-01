f0cb8ece020052169849a2d4c295681a030ac6c9
Markjust1 <novikov711@gmail.com>
2023-02-01 09:44:12 -0700

    add basic structure to myrders comp

--- react-frontend/src/components/profile/MyOrders.jsx
+++ react-frontend/src/components/profile/MyOrders.jsx
@@ -1,6 +1,12 @@
+import "../styles/profile-styles/MyOrders.css";
+
 const MyOrders = () => {
   return (
-    <div>My Orders</div>
+    <>
+    <div className="orders-title">My Orders</div>
+    <div className="orders-title">Saved orders are rendered here </div>
+    </>
+
   )
 };
 
