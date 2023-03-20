ffcb2cc6eae9b8c7fc7f1bb8f26d868058b97fb7
Markjust1 <novikov711@gmail.com>
2023-03-20 09:20:50 -0600

    implement cart item

--- react-frontend/src/components/cart/CartItem.jsx
+++ react-frontend/src/components/cart/CartItem.jsx
@@ -3,7 +3,10 @@ import "../styles/cart-styles/Cart.css";
 const Cart_Item = () => {
   return (
     <div className="cart-item">
-
+      <div className="cart-item-title">Frostmourne</div>
+      <div className="cart-item-platform">Playstation</div>
+      <div className="cart-item-price">$300</div>
+      <div className="cart-remove-button">Remove</div>
     </div>
   )
 };
