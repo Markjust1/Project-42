222eef0a9628bc4f7840f1b20b299c86d070df91
Markjust1 <novikov711@gmail.com>
2023-03-20 12:43:29 -0600

    add description to cart items

--- react-frontend/src/components/cart/CartItem.jsx
+++ react-frontend/src/components/cart/CartItem.jsx
@@ -3,8 +3,11 @@ import "../styles/cart-styles/Cart.css";
 const Cart_Item = () => {
   return (
     <div className="cart-item">
-      <div className="cart-item-title">Frostmourne</div>
-      <div className="cart-item-platform">Playstation</div>
+      <div className="cart-area">
+        <div className="cart-item-title">Frostmourne</div>
+        <div className="cart-item-platform">Playstation</div>
+      </div>
+      <div className="cart-item-description">A rare collectible. Get it while you can</div>
       <div className="cart-item-price">$300</div>
       <div className="cart-remove-button">Remove</div>
     </div>
