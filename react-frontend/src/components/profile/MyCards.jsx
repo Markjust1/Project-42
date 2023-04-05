49090bda77cc358b31c7c4e73010849aed1e73d7
Markjust1 <novikov711@gmail.com>
2023-04-04 22:39:30 -0600

    render cards

--- react-frontend/src/components/profile/MyCards.jsx
+++ react-frontend/src/components/profile/MyCards.jsx
@@ -1,12 +1,14 @@
-const MyCards = () => {
+const MyCards = (props) => {
+
   return (
-    <div>
-      <div>
-        <div className="card-title">**** **** **** 1234</div>
+    <>
+      <div className="card-container">
+        <div className="card-title">**** **** **** {props.cardNumber.toString().substr(-4)}</div>
+        <div className="card-title">{props.expiryDate.toString().substr(0,2)}/{props.expiryDate.toString().substr(-2)}</div>
+      </div>
         <div className="edit-button">EDIT</div>
         <div className="close-button">DELETE</div>
-      </div>
-    </div>
+    </>
   )
 };
 
--- react-frontend/src/components/profile/MyWallet.jsx
+++ react-frontend/src/components/profile/MyWallet.jsx
@@ -5,27 +5,36 @@ import MyCards from "./MyCards";
 import { useState } from "react";
 
 const MyWallet = (props) => {
+  // console.log(props.userData.cardData)
   const [showAddCard, setShowAddCard] = useState(false);
-
+  const data = props.userData.cardData;
+  console.log(data)
   const addCardHandler = () => {
-    console.log(showAddCard)
+    console.log(showAddCard);
     setShowAddCard(!showAddCard);
   };
   return (
     <>
       <div className="wallet-title">My Wallet</div>
       <div className="wallet-container">
-      {!showAddCard ? (
-        <>
-            <MyCards />
-          <div className="edit-profile centered" onClick={addCardHandler}>
-            Add New Card
-          </div>
-        </>
-      ) : (
-        <AddCard userData={props.userData} close={addCardHandler}/>
+        {!showAddCard ? (
+          <>
+            {data.map((el) => (
+              <MyCards
+                key={el.cardNumber}
+                cardNumber={el.cardNumber}
+                expiryDate={el.expiryDate}
+                />
+            )
+            )}
+            <div className="edit-profile centered" onClick={addCardHandler}>
+              Add New Card
+            </div>
+          </>
+        ) : (
+          <AddCard userData={props.userData} close={addCardHandler} />
         )}
-        </div>
+      </div>
     </>
   );
 };
