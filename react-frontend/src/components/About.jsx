f93078909773712a2f508c7a9cff4203aa7a445e
Markjust1 <novikov711@gmail.com>
2023-06-18 12:52:52 -0600

    seciruty text

--- react-frontend/src/components/About.jsx
+++ react-frontend/src/components/About.jsx
@@ -9,15 +9,15 @@ const About = () => {
         and gaming enthusiasts alike. At our site, you can buy and sell in-game
         items with ease, creating a thriving marketplace where virtual treasures
         find their new owners.
-        <br />
-        <br />
+      </p>
+      <p>
         Powered by cutting-edge technologies such as React.JS, Express.JS, and
         MongoDB, our platform delivers a seamless user experience. Whether
         you're a seasoned gamer or just starting out, our user-friendly
         interface makes it effortless to navigate, list, and discover a wide
         array of in-game items.
-        <br />
-        <br />
+      </p>
+      <p>
         Join us today and embark on an extraordinary gaming journey where
         virtual dreams become reality. Start exploring, trading, and connecting
         on our E-commerce site, where gamers unite to make their virtual worlds
--- react-frontend/src/components/Security.jsx
+++ react-frontend/src/components/Security.jsx
@@ -5,17 +5,10 @@ const Security = () => {
     <div className="security-container">
       <h1>Security</h1>
       <p>
-        Atque quasi dignissimos aliquid corrupti nulla dicta architecto vel
-        beatae, iusto harum quidem maxime illum libero?Lorem ipsum dolor sit
-        amet consectetur adipisicing elit. Culpa facere placeat dignissimos rem
-        odit. Atque quasi dignissimos aliquid corrupti nulla dicta architecto
-        vel beatae, iusto harum quidem maxime illum libero? Lorem ipsum dolor
-        sit amet consectetur adipisicing elit. Culpa facere placeat dignissimos
-        rem odit. Atque quasi dignissimos aliquid corrupti nulla dicta
-        architecto vel beatae, iusto harum quidem maxime illum libero?Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. Culpa facere placeat
-        dignissimos rem odit.
+      Security is our utmost priority at our E-commerce platform, where you can confidently buy and sell in-game items. Built with industry-leading technologies such as React.JS, Express.JS, MongoDB, and Stripe for transactions, we have implemented multiple layers of protection to ensure a safe and secure environment for all users.
       </p>
+      <p>We understand the passion and dedication that goes into building your virtual inventory, which is why we prioritize security and reliability. Our secure payment system ensures that transactions are smooth and protected, while our robust backend infrastructure guarantees stability and scalability.</p>
+      <p>With our commitment to security and your cooperation, we strive to provide you with a safe and trustworthy environment to engage in buying and selling in-game items. Your peace of mind is our priority, and we will continue to enhance our security measures to ensure a seamless and protected experience for all our users.</p>
     </div>
   );
 };
