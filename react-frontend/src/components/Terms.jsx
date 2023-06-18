56d0a65b9d208549c076b3ad90264c9335b5b1c3
Markjust1 <novikov711@gmail.com>
2023-06-18 14:00:33 -0600

    terms

--- react-frontend/src/components/Terms.jsx
+++ react-frontend/src/components/Terms.jsx
@@ -1,21 +1,44 @@
-import "./styles/Terms.css";
+// import "./styles/Terms.css";
 
 const Terms = () => {
   return (
     <div className="terms-container">
-      <h1>Terms</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa facere
-        placeat dignissimos rem odit. Atque quasi dignissimos aliquid corrupti
-        nulla dicta architecto vel beatae, iusto harum quidem maxime illum
-        libero?Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-        facere placeat dignissimos rem odit. Atque quasi dignissimos aliquid
-        corrupti nulla dicta architecto vel beatae, iusto harum quidem maxime
-        illum libero?Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Culpa facere placeat dignissimos rem odit. Atque quasi dignissimos
-        aliquid corrupti nulla dicta architecto vel beatae, iusto harum quidem
-        maxime illum libero?
-      </p>
+      <h1>Terms of Use</h1>
+  
+  <h2>1. User Conduct</h2>
+  <p>You acknowledge and agree to use our platform in a lawful and responsible manner. You must not engage in any activities that may:</p>
+  <ol>
+    <li>Violate any applicable laws or regulations.</li>
+    <li>Infringe upon the intellectual property rights of others.</li>
+    <li>Misrepresent yourself or provide false information.</li>
+    <li>Engage in fraudulent or deceptive practices.</li>
+    <li>Interfere with or disrupt the functionality of the platform.</li>
+    <li>Attempt to gain unauthorized access to other users' accounts or personal information.</li>
+    <li>Engage in any activities that may cause harm or damage to our platform or its users.</li>
+  </ol>
+  
+  <h2>2. User Accounts</h2>
+  <p>In order to use certain features of our platform, you may need to create an account. You are responsible for maintaining the confidentiality of your account credentials and for all activities that occur under your account. You must promptly notify us of any unauthorized use or security breaches related to your account.</p>
+  
+  <h2>3. Buying and Selling</h2>
+  <p>Our platform serves as a marketplace for buying and selling in-game items. We do not take ownership or responsibility for the items listed by users. We cannot guarantee the accuracy, quality, or legality of the items listed, and any transactions are solely between buyers and sellers. It is your responsibility to thoroughly review item descriptions, prices, and terms before making a purchase or entering into a transaction.</p>
+  
+  <h2>4. Payments and Transactions</h2>
+  <p>Payments for purchases are processed through our trusted payment gateway partner, Stripe. You agree to comply with the terms and conditions of Stripe regarding payments and transactions. We do not store or have access to your payment information. Any disputes or issues regarding payments should be directed to Stripe.</p>
+  
+  <h2>5. Intellectual Property</h2>
+  <p>The content and materials on our platform, including logos, designs, text, graphics, and images, are protected by intellectual property laws. You agree not to use, reproduce, distribute, or modify any of our copyrighted materials without our prior written consent.</p>
+  
+  <h2>6. Limitation of Liability</h2>
+  <p>To the fullest extent permitted by law, we disclaim any liability for any direct, indirect, incidental, consequential, or exemplary damages arising from your use of the platform or any transactions conducted through it. We do not guarantee the availability, accuracy, or reliability of the platform, and we reserve the right to modify, suspend, or terminate the platform at any time without notice.</p>
+  
+  <h2>7. Modifications to the Terms</h2>
+  <p>We reserve the right to modify or update these Terms of Use at any time. Any changes will be effective immediately upon posting on the platform. It is your responsibility to review the Terms periodically, and your continued use of the platform constitutes acceptance of any modifications.</p>
+  
+  <h2>8. Governing Law and Jurisdiction</h2>
+  <p>These Terms of Use shall be governed by and construed in accordance with the laws of Canada. Any disputes arising from or in connection with these terms shall be subject to the exclusive jurisdiction of the courts of Canada.</p>
+  
+  <p>By using our platform, you agree to abide by these Terms of Use. If you have any questions or concerns, please contact our support team.</p>
     </div>
   );
 };
